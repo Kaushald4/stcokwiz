a86dbcedab9548604769108a54088b4d7db1064c
Kaushal Mehta <kaushalkumar304@gmail.com>
2024-05-15 08:29:49 +0530

    Adding default email, password in login form

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:
-        "https://web-l1evntpzhwbd.up-pl-waw1-k8s-1.apps.run-on-seenode.com/api/v1",
+    baseURL: "https://stockwiz-server.onrender.com/api/v1",
 });
 
 instance.defaults.headers.common["Content-Type"] = "application/json";
